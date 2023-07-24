a12b17fc633f02c29faaa4dc6818e7775b35e431
Juraj Michalek <juraj.michalek@espressif.com>
2023-07-24 14:24:14 +0200

    fix merge-binaries

--- merge-binaries.sh
+++ merge-binaries.sh
@@ -1,4 +1,5 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
+cd build
 esptool.py --chip ESP32S3 merge_bin -o merged-flash.bin --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 bootloader/bootloader.bin 0x10000 wasmachine.bin 0x8000 partition_table/partition-table.bin 0xd000 ota_data_initial.bin 0x810000 storage.bin
 
